3455bf5db30d251a6cb7995e3fb03e54c31d1058
Amaro Mariño <1252707+amalv@users.noreply.github.com>
2019-10-09 16:52:14 +0200

    Staging deploy test

--- pages/index.tsx
+++ pages/index.tsx
@@ -24,7 +24,7 @@ export default function Index() {
     <Container maxWidth="sm">
       <Box my={4}>
         <Typography variant="h4" component="h1" gutterBottom>
-          Testing production environment
+          Testing staging environment
         </Typography>
         <Link href="/about" color="secondary">
           Go to the about page
